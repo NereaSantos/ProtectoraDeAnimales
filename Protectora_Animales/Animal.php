556ded39ef2b10a8c76853782aca802494d042d4
AHouses <127389423+AHouses@users.noreply.github.com>
2023-12-14 09:06:04 +0100

    Update Animal.php bindParams
    Hice mal el commit en casa o algo y no se han guardado los cambios en Animal y en Adopcion

--- Protectora_Animales/Animal.php
+++ Protectora_Animales/Animal.php
@@ -27,15 +27,15 @@ class Animal extends Crud {
     public function crear() {
         $query = "INSERT INTO {$this->tabla} (nombre, especie, raza, genero, color, edad) VALUES (:nombre, :especie, :raza, :genero, :color, :edad)";
         $stmt = $this->conexion->prepare($query);
-        $stmt->bind_param(":nombre", $this->nombre);
-        $stmt->bind_param(":especie", $this->especie);
-        $stmt->bind_param(":raza", $this->raza);
-        $stmt->bind_param(":genero", $this->genero);
-        $stmt->bind_param(":color", $this->color);
-        $stmt->bind_param(":edad", $this->edad);
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":especie", $this->especie);
+        $stmt->bindParam(":raza", $this->raza);
+        $stmt->bindParam(":genero", $this->genero);
+        $stmt->bindParam(":color", $this->color);
+        $stmt->bindParam(":edad", $this->edad);
         $stmt->execute();
 
-        if ($stmt->affected_rows > 0) {
+        if ($stmt->rowCount() > 0) {
             return true;
         } else {
             return false;
@@ -45,16 +45,16 @@ class Animal extends Crud {
     public function actualizar() {
         $query = "UPDATE {$this->tabla} SET nombre = :nombre, especie = :especie, raza = :raza, genero = :genero, color = :color, edad = :edad WHERE id = :id";
         $stmt = $this->conexion->prepare($query);
-        $stmt->bind_param(":nombre", $this->nombre);
-        $stmt->bind_param(":especie", $this->especie);
-        $stmt->bind_param(":raza", $this->raza);
-        $stmt->bind_param(":genero", $this->genero);
-        $stmt->bind_param(":color", $this->color);
-        $stmt->bind_param(":edad", $this->edad);
-        $stmt->bind_param(":id", $this->id);
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":especie", $this->especie);
+        $stmt->bindParam(":raza", $this->raza);
+        $stmt->bindParam(":genero", $this->genero);
+        $stmt->bindParam(":color", $this->color);
+        $stmt->bindParam(":edad", $this->edad);
+        $stmt->bindParam(":id", $this->id);
         $stmt->execute();
 
-        if ($stmt->affected_rows > 0) {
+        if ($stmt->rowCount() > 0) {
             return true;
         } else {
             return false;
