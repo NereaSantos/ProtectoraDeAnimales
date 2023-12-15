e86346e54f49c3c6d28ba46ba5ebe25a38c5d649
Samuel Guerrero <127390653+samurex777@users.noreply.github.com>
2023-12-15 13:17:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="opciones.css">
+    <title>Tablas</title>
+</head>
+<body>
+    <button onclick="window.location.href='./controladores/adopcion_controlador.php'">Adopcion</button><br>
+    <button onclick="window.location.href='./controladores/usuarios_controlador.php'">Usuarios</button><br>
+    <button onclick="window.location.href='./controladores/animal.php'">Animales</button><br>
+</body>
+</html>
